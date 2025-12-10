be0221cc80dd7923e690107a4cb86cf61de8bbd7
TNlap <phnt0604@gmail.com>
2025-12-10 13:10:15 +0700

    update theme

--- inc/custom-post-type/product.php
+++ inc/custom-post-type/product.php
@@ -1,25 +1,25 @@
 <?php
 add_action('init', function () {
 	$labels = array(
-		'name' => __('Products', 'mood'),
-		'singular_name' => __('Product', 'mood'),
-		'add_new' => __('Add new', 'mood'),
-		'add_new_item' => __('Add new product', 'mood'),
-		'edit_item' => __('Adjust product', 'mood'),
-		'new_item' => __('New product', 'mood'),
-		'view_item' => __('View product', 'mood'),
-		'search_items' => __('Search product', 'mood'),
-		'not_found' => __('Not found product', 'mood'),
-		'not_found_in_trash' => __('Not found in trash product', 'mood'),
-		'all_items' => __('All product', 'mood'),
-		'archives' => __('Archives product', 'mood'),
-		'attributes' => __('Attributes product', 'mood'),
-		'insert_into_item' => __('Insert into product', 'mood'),
-		'uploaded_to_this_item' => __('Uploaded to this product', 'mood'),
+		'name' => __('Products', 'mood-theme'),
+		'singular_name' => __('Product', 'mood-theme'),
+		'add_new' => __('Add new', 'mood-theme'),
+		'add_new_item' => __('Add new product', 'mood-theme'),
+		'edit_item' => __('Adjust product', 'mood-theme'),
+		'new_item' => __('New product', 'mood-theme'),
+		'view_item' => __('View product', 'mood-theme'),
+		'search_items' => __('Search product', 'mood-theme'),
+		'not_found' => __('Not found product', 'mood-theme'),
+		'not_found_in_trash' => __('Not found in trash product', 'mood-theme'),
+		'all_items' => __('All product', 'mood-theme'),
+		'archives' => __('Archives product', 'mood-theme'),
+		'attributes' => __('Attributes product', 'mood-theme'),
+		'insert_into_item' => __('Insert into product', 'mood-theme'),
+		'uploaded_to_this_item' => __('Uploaded to this product', 'mood-theme'),
 	);
 
 	$args = array(
-		'label' => __('Product', 'mood'),
+		'label' => __('Product', 'mood-theme'),
 		'labels' => $labels,
 		'public' => true,
 		'publicly_queryable' => true,
@@ -42,17 +42,17 @@ add_action('init', function () {
 
 	// Register Product Category Taxonomy
 	$tax_labels = array(
-		'name' => __('Product Categories', 'mood'),
-		'singular_name' => __('Product Category', 'mood'),
-		'search_items' => __('Search Categories', 'mood'),
-		'all_items' => __('All Categories', 'mood'),
-		'parent_item' => __('Parent Category', 'mood'),
-		'parent_item_colon' => __('Parent Category:', 'mood'),
-		'edit_item' => __('Edit Category', 'mood'),
-		'update_item' => __('Update Category', 'mood'),
-		'add_new_item' => __('Add New Category', 'mood'),
-		'new_item_name' => __('New Category Name', 'mood'),
-		'menu_name' => __('Categories', 'mood'),
+		'name' => __('Product Categories', 'mood-theme'),
+		'singular_name' => __('Product Category', 'mood-theme'),
+		'search_items' => __('Search Categories', 'mood-theme'),
+		'all_items' => __('All Categories', 'mood-theme'),
+		'parent_item' => __('Parent Category', 'mood-theme'),
+		'parent_item_colon' => __('Parent Category:', 'mood-theme'),
+		'edit_item' => __('Edit Category', 'mood-theme'),
+		'update_item' => __('Update Category', 'mood-theme'),
+		'add_new_item' => __('Add New Category', 'mood-theme'),
+		'new_item_name' => __('New Category Name', 'mood-theme'),
+		'menu_name' => __('Categories', 'mood-theme'),
 	);
 
 	$tax_args = array(
@@ -100,39 +100,39 @@ add_action('acf/init', function () {
 	if (!function_exists('acf_add_local_field_group')) return;
 	acf_add_local_field_group(array(
 		'key' => 'group_product_fields',
-		'title' => __('Product', 'mood'),
+		'title' => __('Product', 'mood-theme'),
 		'fields' => array(
 			array(
 				'key' => 'field_product_image',
-				'label' => __('Image', 'mood'),
+				'label' => __('Image', 'mood-theme'),
 				'name' => 'product_image',
 				'type' => 'image',
 				'default_value' => '',
 			),
 			array(
 				'key' => 'field_product_name',
-				'label' => __('Name', 'mood'),
+				'label' => __('Name', 'mood-theme'),
 				'name' => 'product_name',
 				'type' => 'text',
 				'default_value' => '',
 			),
 			array(
 				'key' => 'field_product_price',
-				'label' => __('Price', 'mood'),
+				'label' => __('Price', 'mood-theme'),
 				'name' => 'product_price',
 				'type' => 'number',
 				'default_value' => '',
 			),
 			array(
 				'key' => 'field_product_description',
-				'label' => __('Description', 'mood'),
+				'label' => __('Description', 'mood-theme'),
 				'name' => 'product_description',
 				'type' => 'textarea',
 				'default_value' => '',
 			),
 			array(
 				'key' => 'field_product_taxonomy',
-				'label' => __('Category', 'mood'),
+				'label' => __('Category', 'mood-theme'),
 				'name' => 'product_taxonomy',
 				'type' => 'taxonomy',
 				'taxonomy' => 'product_category',
